9d3578006f5f6a624832fa1e979ce704f4ced159
Chris Knight Johnson <cknightjohnson@gmail.com>
2022-02-11 13:30:17 -0800

    add facebook sdk third party script to Head component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,20 @@
+import Head from 'next/head'
 import Link from 'next/link'
+import Script from 'next/script'
 
 export default function FirstPost() {
     return (
-    <div>
+    <>
+        <Head>
+            <title>First Post</title>
+        </Head>
+        <Script
+            src="https://connect.facebook.net/en_US/sdk.js"
+            strategy="lazyOnload"
+            onLoad={() => 
+                console.log('facebook sdk script loaded')
+            }
+        />
         <h1>First Post</h1>
         <h2>
             <Link href="/">
