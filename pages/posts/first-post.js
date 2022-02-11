0a6f17f54e62e20b33f0f5f7384a06d7c5d413d9
Chris Knight Johnson <cknightjohnson@gmail.com>
2022-02-11 13:29:38 -0800

    move h2 outside Link

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -4,9 +4,11 @@ export default function FirstPost() {
     return (
     <div>
         <h1>First Post</h1>
-        <Link href="/">
-            <a><h2>Go back to home!</h2></a>
-        </Link>
-    </div>
+        <h2>
+            <Link href="/">
+                <a>Go back to home!</a>
+            </Link>
+        </h2>
+    </>
     )
 }
