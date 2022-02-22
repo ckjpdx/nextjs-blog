d7708fa6b8e26df0031ac36a75061d780519bb01
Chris Knight Johnson <cknightjohnson@gmail.com>
2022-02-21 20:14:56 -0800

    Update index.js
    remove unneeded import

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 import Date from '../components/date'
 import Head from 'next/head'
-import Hello from './/api/hello'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
